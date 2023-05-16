4557a1c0cfbfc2c4eed74d57a9c86b89257d371e
Giovanni Valentini <61738817+Giovalentini@users.noreply.github.com>
2023-05-16 15:01:20 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -12,4 +12,14 @@ df = pd.read_csv(github_data_path + 'govgr_data.csv')
 # show df in app
 st.dataframe(df)
 
-st.download_button("download", df, file_name="test.csv")
+# Create a button to download the DataFrame
+def download_dataframe(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="data.csv">Download CSV file</a>'
+    return href
+
+st.dataframe(df)
+
+if st.button('Download'):
+    st.markdown(download_dataframe(df), unsafe_allow_html=True)
